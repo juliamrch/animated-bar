96819ce158bbd6c15ce3de91f64e42bb8fbe4159
Julia March <101819212+juliamrch@users.noreply.github.com>
2024-03-18 16:06:31 +0100

    Update hugo.sh

--- hugo.sh
+++ hugo.sh
@@ -1,4 +1,9 @@
-wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
-tar -xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
-chmod +x ./hugo
-./hugo --gc --minify --destination public
+HUGO_VERSION="0.121.1"
+HUGO_URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+DEST_BIN="${HOME}/.local/bin"
+FILENAME="hugo.tar.gz"
+# Download Hugo Extended and place it in a folder in the $PATH
+curl --create-dirs -s -L -o ${DEST_BIN}/${FILENAME} ${HUGO_URL}
+cd ${DEST_BIN}
+tar xvf ${FILENAME} -C ${DEST_BIN}
+rm ${FILENAME}
